4b18b7e2f807d0239b6dca51ce9f276cc1bed689
Chebukin Matvey <matveynanaren@gmail.com>
2019-03-02 21:26:40 +0200

    initial commit

--- huffman/huffman.cpp
+++ huffman/huffman.cpp
@@ -1,6 +1,8 @@
+#include <iostream>
 #include "huffman.h"
 #include<map>
 #include <vector>
+#include <fstream>
 
 // char -> frequency
 std::map<char,int> table;
@@ -13,3 +15,10 @@ void huffman::compress(std::string file_in,std::string file_out){
 
 }
 
+
+
+
+std::vector<char> read_data(std::string filename){
+    std::ifstream input(filename,std::ios_base::binary);
+     
+}
