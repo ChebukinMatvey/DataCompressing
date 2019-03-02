11cfea1f70f433e4d6afebeff297f241015eeda9
Chebukin Matvey <matveynanaren@gmail.com>
2019-03-02 21:52:19 +0200

    read from file to vector feature added

--- huffman/huffman.cpp
+++ huffman/huffman.cpp
@@ -3,22 +3,38 @@
 #include<map>
 #include <vector>
 #include <fstream>
+#include <assert.h>
 
-// char -> frequency
+
+# define claster 4096
+
+// char -> how much
 std::map<char,int> table;
 
-std::vector<char> read_data(std::string);
+std::vector<char>* read_data(std::string);
 
 
 
 void huffman::compress(std::string file_in,std::string file_out){
+    std::vector<char>* data = read_data(file_in);
 
 }
 
 
 
 
-std::vector<char> read_data(std::string filename){
-    std::ifstream input(filename,std::ios_base::binary);
-     
+std::vector<char>* read_data(std::string filename){
+    std::ifstream file(filename,std::ios_base::binary | std::ios_base::ate);
+    long length = file.tellg();
+    file.seekg(std::ios_base::beg);
+    assert(file.is_open());
+    char* buff = new char[length]; // init pointer
+    auto * data = new std::vector<char>();
+    for(int i=0;!file.eof();i++){
+        file.read(&buff[i*claster],claster);
+    }
+    data->assign(buff,buff+length);
+
+    delete buff;
+    return data;
 }
--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include "static_code_len/static_code_len.h"
-
+#include "huffman/huffman.h"
 
 
 int main(int argc,char** argv) {
-    static_code::compress("../data","../out");
-    static_code::decompress("../out","../decompressed_data");
+    huffman::compress("../data","../out");
+//    static_code::decompress("../out","../decompressed_data");
     return 0;
 }
 
